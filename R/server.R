71fd0bd4c8e18b79784346d2a7098bf9f7f1ed54
Peter Solymos <psolymos@gmail.com>
2020-05-01 01:07:11 -0600

    Shiny app

--- R/server.R
+++ R/server.R
@@ -1,23 +1,72 @@
 server <- function(input, output) {
 
-    ## set file size limit >5MB here
-    options(shiny.maxRequestSize=200*1024^2)
+    output$plot_demogr <- renderPlot({
+        req(x)
+        xx <- if (input$zone == "Total")
+            x else x[x$zone==input$zone,]
+        vct <- Xtab(~ date + gender, xx)
+        vct[vct==0] <- 1
+        vsm <- Xtab(ages ~ date + gender, xx)
+        v <- vsm / vct
+        vv <- mefa4::Melt(as(v, "dgCMatrix"))
+        colnames(vv) <- c("Date", "Gender", "Age")
+        vv$Date <- as.Date(vv$Date)
 
-    output$plot <- renderPlot({
-        iv1 <- x %[]% input$bb_x & y %[]% input$bb_y
-        iv2 <- x %[]% input$bb_y & y %[]% input$bb_x
-        iv3 <- d %()% input$bb_d
-        op <- par(mfrow=c(1,2))
-        plot(x, y, pch = 19, cex = 0.25, col = iv1 + iv2 + 3,
-            main = "Intersecting bounding boxes")
-        plot(x, y, pch = 19, cex = 0.25, col = iv3 + 1,
-            main = "Deck the halls:\ndistance range from center")
-        par(op)
+        p <- ggplot(vv,
+                aes(x=Date, y=Age, color=Gender)) +
+            geom_line() +
+            geom_smooth(method = 'gam')
+        if (input$interv1)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[1])) +
+                geom_vline(xintercept=as.Date(names(interv)[1])+14, lty=2)
+        if (input$interv2)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[2])) +
+                geom_vline(xintercept=as.Date(names(interv)[2])+14, lty=2)
+        if (input$interv3)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[3])) +
+                geom_vline(xintercept=as.Date(names(interv)[3])+14, lty=2)
+        if (input$interv4)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[4])) +
+                geom_vline(xintercept=as.Date(names(interv)[4])+14, lty=2)
+        p
     })
 
-    output$fileinfo <- renderPrint({
-        req(input$file)
-        cat("Size of", input$file$name, "=",
-            round(input$file$size/1024^2, 3), "MB")
+    output$plot_new <- renderPlot({
+        req(AB)
+        p <- ggplot(AB[AB$Zone==input$zone,],
+                aes(x=Date, y=New)) +
+            geom_line() +
+            geom_smooth(method = 'gam')
+        if (input$interv1)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[1])) +
+                geom_vline(xintercept=as.Date(names(interv)[1])+14, lty=2)
+        if (input$interv2)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[2])) +
+                geom_vline(xintercept=as.Date(names(interv)[2])+14, lty=2)
+        if (input$interv3)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[3])) +
+                geom_vline(xintercept=as.Date(names(interv)[3])+14, lty=2)
+        if (input$interv4)
+            p <- p +
+                geom_vline(xintercept=as.Date(names(interv)[4])) +
+                geom_vline(xintercept=as.Date(names(interv)[4])+14, lty=2)
+        p
     })
+
+    output$plot_pred <- renderPlot({
+        req(ABw)
+        i <- input$zone
+        z <- ets(ABw[[i]])
+        plot(forecast(z, 14), main=colnames(ABw)[i],
+             xlab="Days since March 6", ylab="Daily new cases")
+    })
+
+
 }
--- R/ui.R
+++ R/ui.R
@@ -1,24 +1,58 @@
-ui <- fluidPage(
-  titlePanel("intrval example with shiny"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bb_x", "x value:",
-        min=min(x), max=max(x), value=range(x),
-        step=round(diff(range(x))/20, 1), animate=TRUE
+ui <- material_page(
+  include_nav_bar=FALSE,
+  background_color="white",
+  font_color="black",
+  material_row(
+      material_column(
+        width=4,
+        material_dropdown(
+          input_id = "zone",
+          label = "Zones",
+          choices = c("Total", "Calgary", "Edmonton", "Central",
+                      "North", "South"),
+          selected = "Total",
+          color="#64b5f6"
+        )
       ),
-      sliderInput("bb_y", "y value:",
-        min = min(y), max = max(y), value = range(y),
-        step=round(diff(range(y))/20, 1), animate=TRUE
-      ),
-      sliderInput("bb_d", "radial distance:",
-        min = 0, max = max(d), value = c(0, max(d)/2),
-        step=round(max(d)/20, 1), animate=TRUE
-      ),
-      fileInput("file", "choose file")
+      material_column(
+        width=8,
+        material_column(
+          width=4,
+          material_checkbox("interv1",
+                              "No gathering of more than 250 people"),
+          material_checkbox("interv2",
+                              "Schools are closed")
+        ),
+        material_column(
+          width=4,
+          material_checkbox("interv3",
+                              "AHS changed testing"),
+          material_checkbox("interv4",
+                              "High River beef processing plant closed")
+        )
+      )
+  ),
+  material_tabs(
+    tabs = c(
+      "New cases" = "new_cases",
+      "Forecast" = "forecast",
+      "Demography" = "demography"
     ),
-    mainPanel(
-      plotOutput("plot"),
-      verbatimTextOutput("fileinfo")
-    )
+    color="blue"
+  ),
+  material_tab_content(
+    tab_id = "new_cases",
+    plotOutput("plot_new"),
+    tags$p("The number of daily new cases is shown. Intervention dates (solid line) are displayed with a 2-week shift (broken line).")
+  ),
+  material_tab_content(
+    tab_id = "forecast",
+    plotOutput("plot_pred"),
+    tags$p("The number of daily new cases is forecast 2 weeks into the future using exponential smoothing state space model. Shaded areas represent 95% and 80% prediction intervals.")
+  ),
+  material_tab_content(
+    tab_id = "demography",
+    plotOutput("plot_demogr"),
+    tags$p("Average age (lower end of age classes averaged) by date. Intervention dates (solid line) are displayed with a 2-week shift (broken line).")
   )
 )
