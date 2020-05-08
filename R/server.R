b31be8523755beccf4488782b0d6ed00042350fc
Peter Solymos <psolymos@gmail.com>
2020-05-07 22:45:36 -0600

    interactive map + plot

--- R/server.R
+++ R/server.R
@@ -94,6 +94,20 @@ server <- function(input, output) {
             NULL else input$zone
         make_map(as.character(input$date), zone, !input$incidence)
     })
+
+    i.active <- NULL
+    makeReactiveBinding('i.active')
+    ## use input$map_marker_mouseover for hover
+    observeEvent(input$map_shape_click,{
+        id <- input$map_shape_click
+        #pt <- data.frame(X=id$lng, Y=id$lat)
+        #coordinates(pt) <- ~ X+Y
+        #proj4string(pt) <- proj4string(ABsp)
+        i.active <<- as.character(over(
+            SpatialPoints(data.frame(X=id$lng, Y=id$lat), CRS(proj4string(ABsp))), ABsp)$ID)
+        print(i.active)
+    })
+
     output$plot_time <- renderPlot({
         req(AA, A)
         zone <- if (input$zone == "Total")
@@ -105,10 +119,15 @@ server <- function(input, output) {
             Cases=as.numeric(dat),
             Date=as.Date(rep(colnames(dat), each=nrow(dat))),
             Area=rep(rownames(dat), ncol(dat)))
-        p <- ggplot(vv, aes(x=Date, y=Cases, group=Area, color=Area)) +
-            geom_line(show.legend = FALSE) +
+        p <- ggplot(vv, aes(x=Date, y=Cases, group=Area)) +
+            geom_line(colour="grey", show.legend = FALSE) +
             geom_vline(xintercept=input$date) +
             ylab(if (input$incidence) "Cases / 1000 people" else "Number of cases")
+        if (!is.null(i.active))
+            p <- p + geom_line(
+                aes(x=Date, y=Cases), data=vv[vv$Area == i.active, ],
+                colour="blue", show.legend = FALSE, lwd=2) +
+            labs(title=i.active)
         p
     })
 
